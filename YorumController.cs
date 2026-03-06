64fa4cc5838319489f2c5ec491f254594daf1020
AliMertTosun <tosunalimert470@gmail.com>
2026-03-06 16:03:01 +0300

    Add files via upload

---
+++ YorumController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AliMertBayrak.Models;
+using System.Linq;
+
+
+
+namespace AliMertBayrak.Controllers
+{
+    public class YorumController : Controller
+    {
+        private readonly BayrakContext _context;
+
+        public YorumController(BayrakContext context)
+        {
+            _context = context;
+        }
+
+        // FORM SAYFASI
+        public IActionResult Ekle()
+        {
+            return View();
+        }
+
+        // KAYDETME
+        [HttpPost]
+        public IActionResult Ekle(Yorum yorum)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Yorumlar.Add(yorum);
+                _context.SaveChanges();
+
+                return RedirectToAction("Liste");
+            }
+
+            return View(yorum);
+        }
+
+        // LİSTELEME
+        public IActionResult Liste()
+        {
+            var yorumlar = _context.Yorumlar
+                                   .OrderByDescending(x => x.Tarih)
+                                   .ToList();
+
+            return View(yorumlar);
+        }
+    }
+}
