1d9cd690301a2b21f8c783c4f86d2584d33a18c9
AliMertTosun <tosunalimert470@gmail.com>
2026-03-10 11:31:21 +0300

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+using System.Diagnostics;
+using AliMertBayrak.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AliMertBayrak.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Anlam()
+        {
+            return View();
+        }
+        public IActionResult Olculer()
+        {
+            return View();
+        }
+        public IActionResult Kanun()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Hakkimizda()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
